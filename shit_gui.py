e1c2fad0cfe6567c61daff8c364dbf1abfc3f0da
spacedude609 <jwmidds@outlook.com>
2021-08-23 12:05:43 -0400

    Add code
    Added py scripts and assets

---
+++ shit_gui.py
@@ -0,0 +1,38 @@
+from guizero import App, Text, PushButton, ButtonGroup
+from sys import exit
+from winsound import *
+def e():
+    app.destroy()
+    exit(["END"])
+def BEPIS():
+    q1 = app.question("?", "?", "Type Something...")
+    if q1 == "" or q1=="Type Something...":
+        app.warn("WARNING!", "You didn't type anything!, Face the consequences...")
+        app.error("", "")
+        app.error("", "There was a problem displaying this popup. Please try again.")
+        app.error("Error", "There was a problem displaying the title of the previous popup. Please try again.")
+        e()
+    elif q1 == "butt jimm":
+        PlaySound('Uh_Oh.wav', SND_FILENAME)
+        app.error("eqdhdueehd4gdt3r45retw3e73de63et36ete36te63et3hhudwudhw", "ERR_BUTT_JIMMgedygdetdtefdtedtedftefdtefdtefdtefdtefdetdfetdyehdyegvfrgfyrhHDDHDYDGEYDGEYDECMFMVVNFHCGETFDEFDTEDTDFEDHEFURHGURHFURHFURHFRUFURHFRKKYHJYIHJYIHJIYJHIYJHYIHJIJHAFAGAGAGAGAGGAGAHDHRFUHYGEYGDEYGEDYGEDYGEDYGEDhshshgfgfr")
+        e()
+    elif q1 == "yup":
+       app.info("Congrats!", "You did it!")
+       right = Text(app, text="Huh, guess you're right...", size=10, font="Comic Sans MS", color="black")
+       White.destroy()
+       bepis.destroy()
+       BONER()
+def BONER():
+    bonerc = ButtonGroup(app, options=[["What", "W"], ["Am", "A"], ["I", "I"], ["Doing?", "D"]], selected="W")
+    what = app.question("Where am I?", "Who am I?", initial_value="What am I?")
+    if what == "Why?":
+        app.yesno("Question...", "Am I decent?")
+app = App(title="What the heck is this?", width=6, height=9, layout='auto')
+bepis = PushButton(app, command=BEPIS, text="Well this isn't right")
+bepis.bg = "green"
+White = Text(app, text="This button should be white!", size=10, font="Arial", color="black")
+bye = PushButton(app, e, text="Quit", align="right")
+bye.bg = "red"
+app.set_full_screen()
+app.info("IMPORTANT!", "Press Esc. to exit full screen")
+app.display()
---
+++ uh_oh_spaghetti0s.py
@@ -0,0 +1,4 @@
+from winsound import *
+while True:
+# Play the sound file forever
+    PlaySound('Uh_Oh.wav', SND_FILENAME)
